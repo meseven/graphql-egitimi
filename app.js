e8c06cc1d98c5352a8aa5e373658c5cc841bfec9
Mehmet Seven <mehmetseven0@gmail.com>
2018-10-08 00:22:20 +0300

    scalar types

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
 
+// String, Int, ID, Boolean, Float
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
   type Query {
+  	id: ID!
     name: String
     surname: String
     age: Int
+    isAdmin: Boolean
+    score: Float
   }
 `;
 
 // Provide resolver functions for your schema fields
 const resolvers = {
 	Query: {
+		id: () => 'assndkjasd123',
 		name: () => 'Mehmet',
 		surname: () => 'Seven',
-		age: () => 26
+		age: () => 26,
+		isAdmin: () => false,
+		score: () => 4.6
 	},
 };
 
